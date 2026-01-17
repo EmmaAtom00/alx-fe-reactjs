eff84640e98365899b36d70311e087ad298ab2e2
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2026-01-17 15:36:51 +0100

    usecontext in the userprofile

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@ import UserContext from "./components/UserContext";
 import ProfilePage from "./ProfilePage";
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com", age:54, bio:"Software developer" };
 
   return (
     <UserContext.Provider value={{ userData }}>
