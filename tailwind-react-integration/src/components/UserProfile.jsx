aa61c39de6e5db9cc0defb80dbc9e3f03876e5a6
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2026-02-08 01:15:34 +0100

    adding interactivity and transition

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 p-2 sm:max-w-xs md:max-w-sm">
+    <div className="user-profile bg-gray-100 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 p-2 sm:max-w-xs hover:shadow-xl md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4 md:text-xl sm:text-lg">
+      <h1 className="text-xl text-blue-800 my-4 md:text-xl sm:text-lg hover:text-blue-500 transition-colors duration-300">
         John Doe
       </h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">
